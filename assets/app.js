211242ad7452face989bd0aa74474491ccede9dc
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-12-22 17:13:01 -0500

    add js and moment.js logic

--- assets/app.js
+++ assets/app.js
@@ -8,8 +8,65 @@
         messagingSenderId: "761811730905"
     };
     firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    //Button for adding trains
+$("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#dest-input").val().trim();
+    var trainStart = moment($("#first-time-input").val().trim());
+    var trainFreq = $("#freq-input").val().trim();
+  
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+      name: trainName,
+      destination: trainDest,
+      start: trainStart,
+      frequency: trainFreq
+    };
+  
+    // Uploads data to the database
+    database.ref().push(newTrain);
+  
+    // Logs everything to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.start);
+    console.log(newTrain.frequency);
+  
+    alert("Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#dest-input").val("");
+    $("#first-time-input").val("");
+    $("#freq-input").val("");
+  });
+  
+  //Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var trainStart = childSnapshot.val().start;
+    var trainFreq = childSnapshot.val().frequency;
+  
+    // Train Info
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(trainStart);
+    console.log(trainFreq);
+  
+    // Prettify the train start
+    // var trainStartPretty = moment.unix(trainStart).format("***");
     
-    // Assumptions
+    // Moment.JS logic
     var tFrequency = 3;
 
     // Time is 3:30 AM
@@ -38,3 +95,16 @@
     // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    // Create the new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDest),
+        $("<td>").text(trainFreq),
+        $("<td>").text(nextTrain),
+        $("<td>").text(tMinutesTillTrain),
+      );
+    
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+    });
